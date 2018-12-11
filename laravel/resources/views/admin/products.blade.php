2d9847d0a42116afbaad0582151dbf0f00ed5b01
AndresMndez <andresmndeze@gmail.com>
2018-12-11 00:16:33 -0300

    Cambiando el estilo de las tablas en admin view

--- laravel/resources/views/admin/products.blade.php
+++ laravel/resources/views/admin/products.blade.php
@@ -3,41 +3,45 @@
 
 @section('content')
   <h1>Lista de Productos</h1>
-  <table class="table table-striped table-bordered">
-    <thead>
-      <tr>
-        <th>Título</th>
-        <th>Descripcion</th>
-        <th>Marca</th>
-        <th>Precio</th>
-        <th>Imagenes</th>
-        <th></th>
-      </tr>
-    </thead>
-    <tbody>
-      @foreach ($productos as $producto)
+
+  <div class="table-responsive">
+
+    <table class="table .table-hover table-striped table-bordered">
+      <thead class="thead-dark">
         <tr>
-          <td>{{ $producto->name }}</td>
-          <td>{{ $producto->description }}</td>
-          <td>{{ $producto->brand }}</td>
-          <td>{{ $producto->price }}</td>
-          <td><img src="{{ $producto->image }}" style="width:20px;"></td>
-          <td>
-            <a class="btn btn-primary" href="">
-              <span class="fa fa-pencil-alt"></span>
-            </a>
-            <form action="" method="POST">
-              @csrf
-              @method('DELETE')
-              <button type="submit" class="btn btn-danger">
-                <span class="fa fa-trash"></span>
-              </button>
-            </form>
-          </td>
+          <th scope="col">Título</th>
+          <th scope="col">Descripcion</th>
+          <th scope="col">Marca</th>
+          <th scope="col">Precio</th>
+          <th scope="col">Imagenes</th>
+          <th scope="col"></th>
         </tr>
-      @endforeach
-    </tbody>
-  </table>
+      </thead>
+      <tbody>
+        @foreach ($productos as $producto)
+          <tr>
+            <td>{{ $producto->name }}</td>
+            <td>{{ $producto->description }}</td>
+            <td>{{ $producto->brand }}</td>
+            <td>{{ $producto->price }}</td>
+            <td><img src="{{ $producto->image }}" style="width:20px;"></td>
+            <td>
+              <a class="btn btn-primary" href="">
+                <span class="fa fa-pencil-alt"></span>
+              </a>
+              <form action="" method="POST">
+                @csrf
+                @method('DELETE')
+                <button type="submit" class="btn btn-danger">
+                  <span class="fa fa-trash"></span>
+                </button>
+              </form>
+            </td>
+          </tr>
+        @endforeach
+      </tbody>
+    </table>
+  </div>
     {{$productos->  links()}}
 
   {{-- {{ $productos->links() }} --}}
--- laravel/resources/views/admin/users.blade.php
+++ laravel/resources/views/admin/users.blade.php
@@ -3,43 +3,46 @@
 @section('content')
 
   <h1>Usuarios</h1>
-  <table class="table table-striped table-bordered">
-    <thead>
-      <tr>
-        <th>Nombre</th>
-        <th>Email</th>
-        <th>Tipo</th>
-        <th></th>
-      </tr>
-    </thead>
 
-    <tbody>
-      @foreach ($usuarios as $usuario)
+  <div class="table-responsive">
+    <table class="table .table-hover table-striped table-bordered">
+      <thead class="thead-dark">
         <tr>
-          <td>{{ $usuario->name }}</td>
-          <td>{{ $usuario->email }}</td>
-          <td>
-            @if ($usuario->is_admin == 1)
-              Administrador
-            @else
-              Cliente
-            @endif
-          </td>
-
-          <td>
-            <a class="btn btn-primary" href="">
-              <span class="fa fa-pencil-alt"></span>
-            </a>
-            <form action="" method="POST">
-              @csrf
-              @method('DELETE')
-              <button type="submit" class="btn btn-danger">
-                <span class="fa fa-trash"></span>
-              </button>
-            </form>
-          </td>
+          <th scope="col">Nombre</th>
+          <th scope="col">Email</th>
+          <th scope="col">Tipo</th>
+          <th scope="col"></th>
         </tr>
-      @endforeach
-    </tbody>
-  </table>
+      </thead>
+
+      <tbody>
+        @foreach ($usuarios as $usuario)
+          <tr>
+            <td>{{ $usuario->name }}</td>
+            <td>{{ $usuario->email }}</td>
+            <td>
+              @if ($usuario->is_admin == 1)
+                Administrador
+              @else
+                Cliente
+              @endif
+            </td>
+
+            <td>
+              <a class="btn btn-primary" href="">
+                <span class="fa fa-pencil-alt"></span>
+              </a>
+              <form action="" method="POST">
+                @csrf
+                @method('DELETE')
+                <button type="submit" class="btn btn-danger">
+                  <span class="fa fa-trash"></span>
+                </button>
+              </form>
+            </td>
+          </tr>
+        @endforeach
+      </tbody>
+    </table>
+  </div>
 @endsection
